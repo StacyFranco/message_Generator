860df5d7aea5f1b939519661aa79aef376cc2169
StacyFranco <stacyfranco@hotmail.com>
2023-05-01 16:01:18 -0300

    modified message to be a object. Changed the enurable property ( having problem on the loop with getters and comom properties). Change final appeance ( to not have writting erros in the console). Project Fineshed

--- script.js
+++ script.js
@@ -1,23 +1,85 @@
 // Project Relex Day!!!
 
+
+
 // Data Base for the message Generator:
-const places = ['Beach','Park','Moutain','Lake or River','Waterfall','Farm','Amusement park','Touristic City','Aquarium','museun'];
-const activites = ['truth dare','crafty day','spa day','bake off', 'camping','treasure hunt',
- 'plant a tree','stargaze','workout','water ballon figth','horseback riding','read a book'];
-const foods = ['Picnic','Italian Food','Japanese Food','Mexican Food','Fancy eat',
- 'Pizza','Junk Food','barbecue','street food','Potluck', 'Tematic eating'];
+const messageData = {
+    _places: ['Beach', 'Park', 'Moutain', 'Lake or River', 'Waterfall', 'Farm', 'Amusement park', 'Touristic City', 'Aquarium', 'museun'],
+    _activites: ['truth dare', 'crafty day', 'spa day', 'bake off', 'camping', 'treasure hunt',
+        'plant a tree', 'stargaze', 'workout', 'water ballon figth', 'horseback riding', 'read a book'],
+    _foods: ['Picnic', 'Italian Food', 'Japanese Food', 'Mexican Food', 'Fancy eat',
+        'Pizza', 'Junk Food', 'barbecue', 'street food', 'Potluck', 'Tematic eating'],
+        
+
+    // getters
+    get places() {
+        return this._places;
+    },
+    get activites() {
+        return this._activites;
+    },
+    get foods() {
+        return this._foods;
+    },
+
+    //setters
+    set places(NewPlace) {
+        if (typeof NewPlace === 'string' && NewPlace.length > 0 && !this._places.some((x) => x === NewPlace)) {
+            this._places.push(NewPlace);
+        }
+    },
+    set activites(NewActivite) {
+        if (typeof NewActivite === 'string' && NewActivite.length > 0 && !this._activites.some((x) => x === NewActivite)) {
+            this._activites.push(NewActivite);
+        }
+    },
+    set foods(NewFood) {
+        if (typeof NewFood === 'string' && NewFood.length > 0 && !this._foods.some((x) => x === NewFood)) {
+            this._foods.push(NewFood);
+        }
+    },
+
+}
+
+// changing enurable propetries
+Object.defineProperties(messageData, {
+    _places: {enumerable: false},
+    _activites: {enumerable: false},
+    _foods: {enumerable: false},
+});
 
 
 // Function for randonize message:
-const random = (arr) => arr[Math.floor(Math.random()*(arr.length))]
+const random = (arr) => arr[Math.floor(Math.random() * (arr.length))]
+
+// Function to get the message:
+const message = () => {
+    let aux = [];
+    for (let key in messageData) {
+        aux.push(random(messageData[key]))
+        //console.log(key)
+    }
+    return aux;
+
+}
+
+
+// // Testing the setters
+// console.log(messageData.foods);
+// messageData.foods = 'test';
+// messageData._foods.push('test2')
+// console.log(messageData.foods);
 
 // Choosen the words:
-const place = random(places);
-const activite = random(activites);
-const food = random(foods);
-  
+messageChose = message();
+//console.log(messageChose)
 
 // Writing the complete message:
-console.log('                TAKE A DAY FOR RELEX!                    ')
-console.log('=========================================================')
-console.log(`You should go to a ${place}! Maybe do a ${activite}, and you must try a ${food}!`)
+console.log();
+ console.log('                TAKE A DAY FOR RELEX!                    ')
+ console.log('=========================================================')
+ //console.log(`You should go to a ${messageChose[0]}! Maybe do a ${messageChose[1]}, and you must try a ${messageChose[2]}!`)
+ console.log(`    * Your lucky place for today: ${messageChose[0]}!`);
+ console.log(`    * Maybe do this activite: ${messageChose[1]}!`);
+ console.log(`    * You must try: ${messageChose[2]}!`);
+ console.log();
